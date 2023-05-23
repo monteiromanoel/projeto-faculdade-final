3e8236ac1d495b2a92128d61aa2eef081d336fff
Manoel Monteiro <manoel.monteiro1@outlook.com>
2023-05-23 20:26:11 -0300

    update dashboard page

---
+++ dashboard/orders.js
@@ -0,0 +1,39 @@
+const orders = [
+    { 
+        productName: 'A Revolução dos Bichos',
+        productNumber: '132',
+        paymentStatus: 'Pago',
+        shipping: 'Entregue'
+    },
+    {
+        productName: '1984',
+        productNumber: '131',
+        paymentStatus: 'Recusado',
+        shipping: 'Cancelado'
+    },
+    {
+        productName: 'O Príncipe',
+        productNumber: '149',
+        paymentStatus: 'A Pagar',
+        shipping: 'Pendente'
+    },
+    {
+        productName: 'Meditações',
+        productNumber: '112',
+        paymentStatus: 'Pago',
+        shipping: 'Enviado'
+    },
+    {
+        productName: 'Box O Senhor dos Anéis + Hobbit',
+        productNumber: '092',
+        paymentStatus: 'Pago',
+        shipping: 'Enviado'
+    },
+    {
+        productName: 'Sherlock Holmes - O Cão dos Baskervilles',
+        productNumber: '092',
+        paymentStatus: 'A Pagar',
+        shipping: 'Pendente'
+    }
+
+]
--- dashboard/script.js
+++ dashboard/script.js
@@ -14,4 +14,22 @@ closeBtn.addEventListener('click', () =>{
 //change theme
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+//fill orders in table
+
+orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td class="${order.paymentStatus === 'Recusado' ? 'danger' : order.paymentStatus === 'A Pagar' ? 'warning': 'primary'}">${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Cancelado' ? 'danger' : order.shipping === 'Pendente' ? 'warning': 'primary'}">${order.shipping}</td>
+                        <td class="primary">Detalhes</td>
+                        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
 })
