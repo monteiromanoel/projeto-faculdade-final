e4dc8b70bf589879d22b0942566f1e58d928848e
Manoel Monteiro <manoel.monteiro1@outlook.com>
2023-06-04 20:23:15 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -120,7 +120,7 @@ const validateInputsRegister = () => {
     const registerBirthValue = registerBirth.value.trim();
     const registerPhoneValue = registerPhone.value.trim();
     const registerCepValue = registerCep.value.trim();
-    const standardName = /[^a-zà-ú]/gi;
+    const standardName = /^[A-Za-zÀ-ÖØ-öø-ÿ ]+$/;
 
 
     if (registerEmailValue === "") {
@@ -133,7 +133,7 @@ const validateInputsRegister = () => {
 
     if (registerPass1Value === "") {
         setError(registerPass1, "Insira uma senha");
-    } else if (registerPass1.length < 8) {
+    } else if (registerPass1Value.length < 8) {
         setError(registerPass1, "A senha deve ter no mínimo 8 caracteres");
     } else {
         setSuccess(registerPass1);
@@ -141,7 +141,7 @@ const validateInputsRegister = () => {
 
     if (registerPass2Value === "") {
         setError(registerPass2, "Por favor, confirme sua senha");
-    } else if (registerPass2 !== registerPass1) {
+    } else if (registerPass2Value !== registerPass1) {
         setError(registerPass2, "As senhas não coincidem");
     } else {
         setSuccess(registerPass2);
@@ -149,88 +149,19 @@ const validateInputsRegister = () => {
 
     if (registerNameValue === "") {
         setError(registerName, "Insira seu nome e sobrenome");
+    } else if (!registerNameValue.match(standardName)){
+        setError(registerName, "Nome e/ou Sobrenome Inválidos");
     } else {
         setSuccess(registerName);
     };
 
-};
-
-
-
-
-
-
-/*
-const form = document.getElementById('form');
-const fields = document.querySelectorAll(".required");
-const username = document.getElementById("username");
-const email = document.getElementById("userEmail");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    validateInputs();
-});
-
-const setError = (element, message) => {
-    const clientData = element.parentElement;
-        const errorDisplay = clientData.querySelector('.error')
-
-        errorDisplay.innerText = message;
-        clientData.classList.add('error');
-        clientData.classList.remove('success');
-}
-
-const setSuccess = element => {
-    const clientData = element.parentElement;
-    const errorDisplay = clientData.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    clientData.classList.add('success');
-    clientData.classList.remove('error');
-}
-
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-const validateInputs = () => {
-    const userNameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-
-    if(userNameValue === '') {
-        setError(username, "Requer um nome de usuário");
-    } else {
-        setSuccess(username);
+    if (registerCpfValue === "") {
+        setError(registerCpf, "Insira seu CPF");
+    } else if (registerCpfValue.length < 11) {
+        setError(registerCpf, "Número de CPF inválido, insira novamente.")
     };
 
-    if(emailValue === '') {
-        setError(email, "Requer um E-mail");
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Insira um e-mail válido');
-    } else {
-        setSuccess(email);
-    }
 
-    if(passwordValue === '') {
-        setError(password, "Requer um nome de usuário");
-    } else if (passwordValue.length < 8){
-        setError(password, "A senha deve ter no mínimo 8 caracteres");
-    } else {
-        setSuccess(password);
-    }
-
-    if(password2Value === '') {
-        setError(password2, "Requer uma senha");
-    } else if (password2Value !== passwordValue){
-        setError(password2, "As senhas não coincidem");
-    } else {
-        setSuccess(password2);
-    }
 };
-*/
+
+
