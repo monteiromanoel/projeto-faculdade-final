c2dbd0067940fe8d30fe49952fc59016b08f90ad
Manoel Monteiro <manoel.monteiro1@outlook.com>
2023-06-05 23:09:49 -0300

    desgraça

--- script/script.js
+++ script/script.js
@@ -88,7 +88,7 @@ const validateInputs = () => {
 
 /*------- VALIDATION AREA REGISTER PAGE -------*/
 
-const registerArea = document.getElementById("formRegister");
+const registerArea = document.getElementById('formRegister');
 const registerEmail  = document.getElementById("userEmail");
 const registerPass1 = document.getElementById("password1");
 const registerPass2 = document.getElementById("password2");
@@ -104,6 +104,24 @@ registerArea.addEventListener('submit', e => {
 
     validateInputs();
 });
+
+const setError2 = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+};
+
+const setSuccess2 = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
 
 const isValidEmail2 = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -124,44 +142,43 @@ const validateInputsRegister = () => {
 
 
     if (registerEmailValue === "") {
-        setError(registerEmail, "Insira seu e-mail");
+        setError2(registerEmail, "Insira seu e-mail");
     } else if (!isValidEmail2(registerEmailValue)) {
-        setError(registerEmail, "Insira um e-mail válido");
+        setError2(registerEmail, "Insira um e-mail válido");
     } else {
-        setSuccess(registerEmail);
+        setSuccess2(registerEmail);
     };
-
+    
     if (registerPass1Value === "") {
-        setError(registerPass1, "Insira uma senha");
+        setError2(registerPass1, "Insira uma senha");
     } else if (registerPass1Value.length < 8) {
-        setError(registerPass1, "A senha deve ter no mínimo 8 caracteres");
+        setError2(registerPass1, "A senha deve ter no mínimo 8 caracteres");
     } else {
-        setSuccess(registerPass1);
+        setSuccess2(registerPass1);
     };
 
     if (registerPass2Value === "") {
-        setError(registerPass2, "Por favor, confirme sua senha");
+        setError2(registerPass2, "Por favor, confirme sua senha");
     } else if (registerPass2Value !== registerPass1) {
-        setError(registerPass2, "As senhas não coincidem");
+        setError2(registerPass2, "As senhas não coincidem");
     } else {
-        setSuccess(registerPass2);
+        setSuccess2(registerPass2);
     };
 
     if (registerNameValue === "") {
-        setError(registerName, "Insira seu nome e sobrenome");
+        setError2(registerName, "Insira seu nome e sobrenome");
     } else if (!registerNameValue.match(standardName)){
-        setError(registerName, "Nome e/ou Sobrenome Inválidos");
+        setError2(registerName, "Nome e/ou Sobrenome Inválidos");
     } else {
-        setSuccess(registerName);
+        setSuccess2(registerName);
     };
 
     if (registerCpfValue === "") {
-        setError(registerCpf, "Insira seu CPF");
+        setError2(registerCpf, "Insira seu CPF");
     } else if (registerCpfValue.length < 11) {
-        setError(registerCpf, "Número de CPF inválido, insira novamente.")
+        setError2(registerCpf, "Número de CPF inválido, insira novamente.")
+    } else {
+        setSuccess2(registerCpf);
     };
-
-
 };
-
 
